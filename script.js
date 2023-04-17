558d5b4dbc4a68cfd29a349712cb4ec346df7151
Micael-Macedo <micaelm2009@hotmail.com>
2023-04-17 09:49:04 -0300

    feat: add functional buttons

--- script.js
+++ script.js
@@ -1,35 +1,81 @@
-let ratio = document.getElementById("ratio").checked
-let width = document.querySelector("#width").value
-let height = document.querySelector("#height")
-let file = document.querySelector("#file")
-let quality = document.getElementById("quality").checked
-
+ratio = document.getElementById("ratio")
+width = document.querySelector("#width")
+height = document.querySelector("#height")
+file = document.querySelector("#file")
+quality = document.getElementById("quality")
+ImageWitdh = 0
+ImageHeight = 0
+imageSrc = ""
 file.addEventListener("change", (e) => {
     let userFile = e.target.files[0]
     let fileReader = new FileReader()
     fileReader.onload = () =>{
-        let src = fileReader.result;
-        canvasImage(src)
+        src = fileReader.result
+        let image = document.createElement("img")
+        image.src = src
+        imageSrc = src
+        image.id = "fileImage"
+        image.onload = () => {
+            console.log(image)
+            width.value = image.width
+            ImageWidth = image.width
+            height.value = image.height
+            ImageHeight = image.height
+            $(".drop-content").empty()
+            $(".drop-content").append(image)
+        }
+        
     }
     fileReader.readAsDataURL(userFile);
 })
+ratio.addEventListener("change", verificarRatio)
+quality.addEventListener("change", verificarQuality)
+width.addEventListener("change", changeHeight)
 
-
-function canvasImage(source) {
-    let image = document.createElement("img")
-    image.src = source
-    image.onload = (event) =>{
-        
-        let canvas = document.createElement("canvas");
-        let context = canvas.getContext("2d");
-        context.drawImage(image, 0, 0, width, height)
-
+function verificarRatio() {
+    if(this.checked === true){
+        $("#height").prop("disabled", true) 
+        changeHeight()  
+    }else{
+        $("#height").prop("disabled", false)   
     }
+}
+function changeHeight(){
+    let InputWidth = parseFloat(width.value)
+    console.log(InputWidth)
+    if(ratio.checked === true){
+        height.value = (InputWidth * ImageHeight) / ImageWidth 
+    }
+}
 
-    console.log(ratio)
-    console.log(quality)
-    
-    canvas.width = width.nodeValue()
+function verificarQuality() {
+    return quality.checked;
+}
 
-    context.drawImage(source, 0, 0, canvas.width, canvas.height)
+function returnOriginalSize() {
+    width.value = ImageWidth
+    height.value = ImageHeight
+}
+function baixarImagem() {
+    let image = document.createElement("img")
+    image.src = imageSrc
+    let checkQuality = 0
+
+    if(quality.checked === true){
+        checkQuality = 1
+    }else{
+        checkQuality = 0.7
+    }
+    console.log(checkQuality)
+    let canvas = document.createElement("canvas");
+    canvas.width = parseFloat(width.value)
+    canvas.height = parseFloat(height.value)
+    let context = canvas.getContext("2d");
+    context.drawImage(image, 0, 0, canvas.width, canvas.height)
+
+    let new_image = document.createElement("img")
+    let new_image_url = context.canvas.toDataURL("image/jpeg", checkQuality)
+    new_image.src = new_image_url
+    $("#newImage").empty()
+    $("#newImage").append(new_image)
 }
